c3ad5e32ac75d4608218b28088023c2d79a61d2a
ahmadaw <ahmadaw@post.bgu.ac.il>
2024-02-13 10:28:26 +0200

    assignment 2

---
+++ Project/Tsofen/compare_source_target.py
@@ -0,0 +1,32 @@
+import json
+import os
+import enums
+
+class compare_source_target:
+    def prerequisite(self):
+        return
+    def Run(self,name):
+        return self.run_compare_process(name)
+    def run_compare_process(self,fileName):
+        try:
+            filePath = os.getcwd()  # get current directory
+            for root, dirs, files in os.walk(
+                    filePath):  # start walking toward from the current directory to find the file
+                if fileName in files:
+                    filePath = os.path.join(root, fileName)
+            with open(filePath, 'r', encoding='utf-8') as json_file:
+                data = json.load(json_file)
+            source = None
+            for d in data:
+                if source is None:
+                    source = d
+                else:
+                    for app in data[source]:
+                        if app.find("version") != -1 and data[source][app] != data[d][app]:
+                            return enums.Status.Failure.value
+        except Exception as e:
+            print(f"Error: {e}")
+            return enums.Status.Failure.value
+        return enums.Status.Success.value
+    def send_email(self):
+        return
--- Project/Tsofen/main.py
+++ Project/Tsofen/main.py
@@ -1,28 +1,5 @@
-import json
-import os
-
-
-def compareVersions(name):
-    try:
-        filePath = os.getcwd()  # get current directory
-        for root, dirs, files in os.walk(filePath):  # start walking toward from the current directory to find the file
-            if name in files:
-                filePath = os.path.join(root, name)
-        with open(filePath, 'r', encoding='utf-8') as json_file:
-            data = json.load(json_file)
-        source = None
-        for d in data:
-            if source is None:
-                source = d
-            else:
-                for app in data[source]:
-                    if app.find("version") != -1 and data[source][app] != data[d][app]:
-                        return False
-    except Exception as e:
-        print(f"Error: {e}")
-        return False
-    return True
-
+import compare_source_target
 
 if __name__ == '__main__':
-    print(compareVersions('json_file.json'))
+    CompareClass=compare_source_target.compare_source_target()
+    print(CompareClass.Run('json_file.json'))
